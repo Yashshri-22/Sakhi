77fc4b80072be310016e26f935f7b616f934eac1
Yashshri-22 <yashshri2205@gmail.com>
2026-01-20 15:23:31 +0530

    Design Listen your body page

---
+++ app/src/main/java/com/example/sakhi/ListenYourBodyActivity.java
@@ -0,0 +1,14 @@
+package com.example.sakhi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListenYourBodyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_your_body);
+    }
+}
